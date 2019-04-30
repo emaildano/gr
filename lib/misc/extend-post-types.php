9dba8022c85235502a2f9e4a102b83497b13d0f2
Daniel Olson <emaildano@gmail.com>
2019-04-30 17:35:06 -0400

    updated color

--- lib/misc/extend-post-types.php
+++ lib/misc/extend-post-types.php
@@ -50,12 +50,11 @@ function projects_cpt() {
     'show_in_admin_bar'     => true,
     'show_in_nav_menus'     => false,
     'can_export'            => true,
-    'has_archive'           => 'projects',
-    'rewrite'               => array( 'slug' => 'project' ),
+    'has_archive'           => true,
     'exclude_from_search'   => false,
     'publicly_queryable'    => true,
     'capability_type'       => 'page',
-    'show_in_rest'          => true,
+    'show_in_rest'          => true
   );
   register_post_type( 'projects', $args );
 
